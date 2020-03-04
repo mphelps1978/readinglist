33d9d1365e4c0fc654e9b134a1aa8d59b30521f5
Michael Phelps <michaeljphelps1978@gmail.com>
2020-03-03 21:28:14 -0600

    creates graphQL Server with no schema

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql')
+
+const PORT = process.env.PORT || 4000
+
+const app = express()
+app.use('/graphql', graphqlHTTP({}));
+
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`)
+  })
