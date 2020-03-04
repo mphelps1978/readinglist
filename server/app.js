b8e5152fdb6d76b197a3ce4425c3b4670ea144f7
Michael Phelps <michaeljphelps1978@gmail.com>
2020-03-03 21:46:55 -0600

    creates single rootQuery for a book

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
 
 const PORT = process.env.PORT || 4000
 
 const app = express()
-app.use('/graphql', graphqlHTTP({}));
+app.use('/graphql', graphqlHTTP({schema}));
 
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`)
