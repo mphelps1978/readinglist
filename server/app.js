c905ed90b4b8c67bf780ca6d9d9049c686eca304
Michael Phelps <michaeljphelps1978@gmail.com>
2020-03-03 23:03:34 -0600

    adds authors schema, and rootQuery

--- server/app.js
+++ server/app.js
@@ -5,7 +5,10 @@ const schema = require('./schema/schema')
 const PORT = process.env.PORT || 4000
 
 const app = express()
-app.use('/graphql', graphqlHTTP({schema}));
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
 
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`)
